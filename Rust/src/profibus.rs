3e472e132e55bd6fa7ce9d97498089e785f18cfa
Marcel Maage <mmaage92@googlemail.com>
2023-01-02 14:13:31 +0100

    Backup before trait changes

--- Rust/src/profibus.rs
+++ Rust/src/profibus.rs
@@ -9,24 +9,53 @@ enum DpSlaveState
     Ddxchg = 4// Data exchange
 }
 
+enum StreamState
+{
+    WaitSyn,
+    WaitData,
+    GetData,
+    WaitMinTsdr,
+    SendData
+}
+
 pub struct PbDpSlave{
     buffer: Vec<u8, 255>,
-    state : DpSlaveState,
+    slave_state : DpSlaveState,
+    stream_state : StreamState,
 }
 
 impl PbDpSlave{
     pub fn new() ->Self{
         Self{
             buffer : Vec::<u8, 255>::new(),
-            state : DpSlaveState::Por,
+            slave_state : DpSlaveState::Por,
+            stream_state : StreamState::WaitSyn,
         }
     }
 
-    pub fn handle_data(&mut self, data : u8) {
-        match self.state{
-            DpSlaveState::Ddxchg=>self.buffer.push(data).unwrap(),
-            _=>self.buffer.push(data).unwrap(),
-        }
+    pub fn handle_rx(&mut self, data : u8) {
+        self.buffer.push(data).unwrap();
+
+  // if we waited for TSYN, data can be saved
+  if (StreamState::WaitData == self.stream_state)
+  {
+    self.stream_state = StreamState::GetData;
+  }
+
+  // Einlesen erlaubt?
+  if (StreamState::GetData == self.stream_state)
+  {
+    m_rxBufCnt++;
+
+    // Nicht mehr einlesen als in Buffer reinpasst
+    if (m_rxBufCnt >= m_config.bufSize)
+      m_rxBufCnt--;
+  }
+
+  // Profibus Timer ruecksetzen
+  setTimerCounter(0);
+  clearOverflowFlag();
+
     }
 
     pub fn handle_message_timeout(&mut self)
@@ -34,5 +63,21 @@ impl PbDpSlave{
         let _test = self.buffer.len();
 
         self.buffer.clear();
+    }
+}
+
+pub struct PbDpSlaveStm32f1{
+    buffer: Vec<u8, 255>,
+    slave_state : DpSlaveState,
+    stream_state : StreamState,
+}
+
+impl PbDpSlaveStm32f1{
+    pub fn new() ->Self{
+        Self{
+            buffer : Vec::<u8, 255>::new(),
+            slave_state : DpSlaveState::Por,
+            stream_state : StreamState::WaitSyn,
+        }
     }
 }
